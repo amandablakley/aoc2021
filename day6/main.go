3040db6f21569b8db482cfa3b3a4b8ba2a73e693
Manda <amanda.blakley@tcgplayer.com>
2022-01-05 09:38:49 -0500

    Day 6 part 1 & 2

--- day6/main.go
+++ day6/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
@@ -19,7 +20,55 @@ func main() {
 		log.Println(err)
 	}
 
-	lines := strings.Split(string(input), "\n")
+	lines := strings.Split(string(input), ",")
 
-	fmt.Println(lines)
+	inputMap := make([]int64, 9)
+	for i := 0; i <= 8; i++ {
+		input[i] = 0
+	}
+	for _, value := range lines {
+		val, _ := strconv.ParseInt(value, 10, 16)
+		inputMap[uint(val)] += 1
+	}
+
+	totalDayOne, totalDayTwo := days(inputMap, 80, 256)
+
+	fmt.Println("Part One: ", totalDayOne)
+	fmt.Println("Part Two: ", totalDayTwo)
+
+}
+
+func oneDay(input []int64) []int64 {
+	total := make([]int64, 9)
+	for _, i := range [7]uint{0, 1, 2, 3, 4, 5, 7} {
+		total[i] = input[i+1]
+	}
+	total[6] = input[0] + input[7]
+	total[8] = input[0]
+	return total
+}
+
+func days(input []int64, day1, day2 uint16) (int64, int64) {
+	i := uint16(0)
+
+	for ; i < day1; i++ {
+		input = oneDay(input)
+	}
+
+	totalDayOne := int64(0)
+	for _, value := range input {
+		totalDayOne += value
+	}
+
+	// Part 2
+	for ; i < day2; i++ {
+		input = oneDay(input)
+	}
+
+	totalDayTwo := int64(0)
+	for _, value := range input {
+		totalDayTwo += value
+	}
+
+	return totalDayOne, totalDayTwo
 }
